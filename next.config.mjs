4c534fee58b1b4e226a80cd47c5998b864783f16
sudoxv <salmanajja54@gmail.com>
2025-06-17 18:10:48 +0700

    sudo

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+import { withPlugins } from 'next-compose-plugins';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+export default nextConfig;
