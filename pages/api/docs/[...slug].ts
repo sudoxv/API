666c47ab692983bfcd8944d80ea7b3cc34e773b0
sudoxv <salmanajja54@gmail.com>
2025-06-18 08:52:28 +0700

    Update [...slug].ts

--- pages/api/docs/[...slug].ts
+++ pages/api/docs/[...slug].ts
@@ -1,34 +1,29 @@
+import React from 'react';
 import dynamic from 'next/dynamic';
-
-const SwaggerUI = dynamic(() => import('swagger-ui-react'), { ssr: false });
-
 import 'swagger-ui-react/swagger-ui.css';
 import specs from '../../../swagger';
+const SwaggerUI = dynamic(() => import('swagger-ui-react'), {
+  ssr: false,
+});
 
-const ApiDocs = () => {
+const ApiDocs: React.FC = () => {
     return (
-        <SwaggerUI 
-            spec={specs} 
+        <div style={{ padding: 16 }}>
+        <SwaggerUI
+            spec={specs}
             options={{
                 customCss: `
-                    .swagger-ui .opblock .opblock-summary {
-                        border-radius: 10px;
-                    }
-                    .swagger-ui .opblock .opblock-summary-description {
-                        border-radius: 10px;
-                    }
-                    .swagger-ui .scheme-container {
-                        border-radius: 10px;
-                    }
-                    .swagger-ui .opblock-tag {
-                        border-radius: 10px;
-                    }
+                    .swagger-ui .opblock .opblock-summary,
+                    .swagger-ui .opblock .opblock-summary-description,
+                    .swagger-ui .scheme-container,
+                    .swagger-ui .opblock-tag,
                     .swagger-ui .opblock {
                         border-radius: 10px;
                     }
                 `,
             }}
         />
+        </div>
     );
 };
 
