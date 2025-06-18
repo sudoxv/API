a79826d6afae0c082928872c5958b2902f770df7
sudoxv <salmanajja54@gmail.com>
2025-06-18 10:02:48 +0700

    Update swagger.ts

--- swagger.ts
+++ swagger.ts
@@ -7,7 +7,12 @@ const options = {
         info: {
             title: 'API Documentation',
             version: '1.0.0'
-        }
+        },
+        servers: [
+            {
+                url: 'https://api-fdkf.vercel.app'
+            }
+        ]
     },
     apis: [path.join(process.cwd(), 'pages/api/**/*.ts')]
 }
