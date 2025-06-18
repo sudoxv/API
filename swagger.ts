ffe801ce5b4c58fb3c25bfee0c4a47a5175ffabd
sudoxv <salmanajja54@gmail.com>
2025-06-18 12:22:27 +0700

    Update swagger.ts

--- swagger.ts
+++ swagger.ts
@@ -12,40 +12,8 @@ const options = {
                 url: 'https://api-fdkf.vercel.app'
             }
         ],
-        {
-  "openapi": "3.0.0",
-  "info": {
-    "title": "API Documentation",
-    "version": "1.0.0"
-  },
-  "servers": [
-    {
-      "url": "https://your-vercel-url.vercel.app"
-    }
-  ],
-  "paths": {
-    "/api/stalk/tiktok": {
-      "get": {
-        "summary": "Tiktok Stalker",
-        "parameters": [
-          {
-            "in": "query",
-            "name": "username",
-            "required": true,
-            "schema": {
-              "type": "string"
-            }
-          }
-        ],
-        "responses": {
-          "200": {
-            "description": "Data TikTok"
-          }
-        }
-      }
-    }
-  },
-  apis: ["pages/api/**/*.ts"]
+    },
+    apis: ["pages/api/**/*.ts"]
 }
 
 const specs = swaggerJsdoc(options);
