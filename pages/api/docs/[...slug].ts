16cc34e814d553f256854761288378d1989f0472
sudoxv <salmanajja54@gmail.com>
2025-06-18 08:06:14 +0700

    sudo

---
+++ pages/api/docs/[...slug].ts
@@ -0,0 +1,26 @@
+import SwaggerUI from 'swagger-ui-react';
+import 'swagger-ui-react/swagger-ui.css';
+import specs from '../../../swagger';
+
+function ApiDocs() {
+    return (
+        <SwaggerUI
+            spec={specs}
+            options={{
+                customCss: `
+                    .swagger-ui .opblock .opblock-summary {
+                        border-radius: 10px;
+                    }
+                    .swagger-ui .opblock .opblock-summary-description {
+                        border-radius: 10px;
+                    }
+                    .swagger-ui .scheme-container {
+                        border-radius: 10px;
+                    }
+                `
+            }}
+        />
+    )
+}
+
+export default ApiDocs;
